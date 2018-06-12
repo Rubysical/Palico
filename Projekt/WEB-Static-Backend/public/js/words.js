5fe1d24d8f738e28ae7974df180531a6105edff3
Rubysical <37865483+Rubysical@users.noreply.github.com>
2018-06-12 16:02:20 +0200

    Update

--- Projekt/WEB-Static-Backend/public/js/words.js
+++ Projekt/WEB-Static-Backend/public/js/words.js
@@ -1,32 +1,6 @@
-/*var addWordsUrl = "http://localhost:3000/add-word";
-
-
-window.addEventListener("load", function() { 
-
-    // Dem HTML-Button (name="senden") den Event-Handler: "click" zuweisen 
-    // dieser ruft dann (beim klicken) die Funktion: eintragen() auf. 
-    document.getElementsByName("senden")[0].addEventListener("click", senden); 
-    
-   }); 
-
-function senden(){
-    xhr = new XMLHttpRequest();
-    xhr.open("PUT", addWordsUrl, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    var word = document.getElementById("label1").value;
-    array = [word];
-    var json = JSON.stringify(array);
-    console.log(json);
-    xhr.send(json);
-   
-}
-
-*/
-
 $(document).ready(function(){
 
     $("button").click(function(){
-        alert("asd");
         myFunction();
     });
 });
@@ -45,8 +19,6 @@ $.ajax({
     url: 'http://localhost:3000/add-word',
     data: json
 });
-
-
 
 //TimeOut nicht schön !
 
