2ff4d79620f43ad9f4c3978f5881b4573f3a7803
Ufuk57 <u.demircelik@hotmail.de>
2018-07-15 13:02:07 +0200

    kurze verbesserung

--- Projekt/WEB-Static-Backend/public/js/highscore.js
+++ Projekt/WEB-Static-Backend/public/js/highscore.js
@@ -1,36 +1,24 @@
+
 $(document).ready(function(){
-getScore();
-getWord();
-});
-
-
-function getScore(){
-    $("#highscore tr").remove(); 
+    getScore();
+    myFunction();
+    });
     
-    $.ajax({
-    type: 'GET',
-    url: 'http://localhost:3000/highscore',
-    success: function(data) {
-        for (var key in data) {
-            $('#highscore').append(`<tr> <td>${key}</td> <td>${data[key]}</td> </tr>`);
+    var x ={'testtest':10};
+    
+    
+    function getScore(){
+        $("#highscore tr").remove(); 
+        
+        $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/highscore',
+        success: function(data) {
+            for (var key in data) {
+                $('#highscore').append(`<tr> <td>${key}</td> <td>${data[key]}</td> </tr>`);
+            }
+    
         }
-
+    });
+     
     }
-});
- 
-}
-function getWord(){
-    
-    $.ajax({
-    type: 'GET',
-    url: 'http://localhost:3000/words',
-    success: function(data) {
-        length = data.length;
-        alert(length);
-        randomNumber = Math.floor((Math.random() * length) + 1);
-        alert(data[randomNumber]);
-    }
-    
-});
- 
-}
